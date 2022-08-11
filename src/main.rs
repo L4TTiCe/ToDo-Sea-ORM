1e4e5f61908a0b3c4a68dc1a203b21893d02b2dc
L4TTiCe <dheshan.mail@gmail.com>
2022-08-11 19:00:15 -0400

    Configure Logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 use actix_web::{get, App, HttpServer, Responder, middleware};
 
+#[macro_use]
+extern crate log;
+
 #[get("/up")]
 async fn health_check() -> impl Responder {
+    info!("GET /up Health Check");
     "Server is Up!".to_string()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    
+    pretty_env_logger::init();
+    
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
